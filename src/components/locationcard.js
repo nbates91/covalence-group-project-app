9904a464636dccbb1fc39923eaabff42bbd24ec6
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-01 22:42:56 -0500

    added check icons to active route screen, added navigation to game over screen once user completes a route, added warning to route details screen, button content on route details screen changes depending on which route the user is looking at, etc.

--- src/components/locationcard.js
+++ src/components/locationcard.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { Button, Card, CardItem, Text, Body } from 'native-base';
 import { withNavigation, NavigationActions } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class LocationCard extends Component {
 	render() {
+		if (this.props.addIcon) {
+			return (
+				<Card>
+					<CardItem button onPress={() => this.props.onPress()}>
+						<Body>
+							<Icon name="check" size={25} color="#B3FFAA"/>
+							<Text>{this.props.stop.stops}</Text>
+						</Body>
+					</CardItem>
+				</Card>
+			);
+		}
 		return (
 			<Card>
 				<CardItem button onPress={() => this.props.onPress()}>
