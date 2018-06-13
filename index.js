c29937bb745671ebe74c6a1b3ea57227f8bed580
jrmelton50 <jrmelton@crimson.ua.edu>
2018-06-13 10:36:34 -0500

    removed warnings

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { YellowBox } from 'react-native';
 
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Class RCTCxxModule', 'Module RNFetchBlob requires main queue setup',]);
 
 AppRegistry.registerComponent('BhamBarhopFrontEnd', () => App);
